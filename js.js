8b1664fcae0f31d1148371ef07c365ae7a84a4d8
Nozimjon Olimjonov <97082922+nozimjon1019@users.noreply.github.com>
2022-02-12 15:16:53 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+const boshi = document.querySelector(".boshi");
+const boshi1 = document.querySelector(".boshi1");
+const boshi2 = document.querySelector(".boshi2");
+const boshi3 = document.querySelector(".boshi3");
+const boshi4 = document.querySelector(".boshi4");
+
+function Addstyle() {
+  boshi.classList.toggle("qosh1");
+}
+
+function AddStyel1() {
+  boshi1.classList.toggle("yangiQosh");
+}
+
+function AdStyel2() {
+  boshi2.classList.toggle("uchinchi");
+}
+
+function Addstyele3() {
+  boshi3.classList.toggle("tortinchiqosh");
+}
+
+function Addstyle4() {
+  boshi4.classList.toggle("songisi");
+}
